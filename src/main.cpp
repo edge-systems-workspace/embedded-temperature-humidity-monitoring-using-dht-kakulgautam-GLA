057e633e774f9e4a4847ed7f831037dea427ebf9
kakul-gautam <gautamkakul5@gmail.com>
2026-02-21 20:12:32 +0530

    define DHT sensor type and create DHT object in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -19,9 +19,11 @@
 
 // TODO 2:
 // Define the DHT sensor type (DHT11)
+#define DHTTYPE DHT11
 
 // TODO 3:
 // Create a DHT object using the defined pin and sensor type
+DHT dht(DHTPIN, DHTTYPE);
 
 void setup() {
 
